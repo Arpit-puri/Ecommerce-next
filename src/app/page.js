8fe63a6be3807a388cf37375da8dbce649643679
Arpit <arpit.puri6265@gmail.com>
2023-08-09 21:45:28 +0530

    ss

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import React from "react";
 export default function Home() {
- 
   return (
     <>
       <main suppressHydrationWarning>
         <h1>Changes on way</h1>
+        <h2>Main page</h2>
       </main>
     </>
   );
