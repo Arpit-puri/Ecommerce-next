2cb0b5cf4df6ba4ca6ca133aa13fdf1fae880dc4
Arpit <arpit.puri6265@gmail.com>
2023-08-09 21:50:35 +0530

    title

--- src/app/dashboard/admin/products/page.js
+++ src/app/dashboard/admin/products/page.js
@@ -53,7 +53,7 @@ const Newproduct = () => {
                 >
                   <div className="card m-2" style={{ width: "18rem" }}>
                     <div className="card-body">
-                      <h5 className="card-title">Card title</h5>
+                      <h5 className="card-title">{p.name}</h5>
                       <p className="card-text">{p.description}</p>
                     </div>
                   </div>
