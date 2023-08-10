be805a07f3a3867c588fdd8f30d60f974b1153b0
Arpit <arpit.puri6265@gmail.com>
2023-08-10 21:26:07 +0530

    filters

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@ import React, { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
+import { Prices } from "./components/Prices";
 import Link from "next/link";
-import Checkbox from "antd/es/checkbox/Checkbox";
-import { Radio } from "antd";
+import { Checkbox, Radio } from "antd";
 import Image from "next/image";
 export default function Home() {
   const toastOptions = {
@@ -18,8 +18,11 @@ export default function Home() {
     progress: undefined,
     theme: "light",
   };
-
   const [check, setCheck] = useState([]);
+  const [radio, setRadio] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [total, setTotal] = useState(0);
   const [products, setProducts] = useState([]);
   const [category, setCategory] = useState([]);
 
@@ -62,7 +65,7 @@ export default function Home() {
     if (value) {
       all.push(id);
     } else {
-      all.filter((c) => c !== id);
+      all = all.filter((c) => c !== id);
     }
     setCheck(all);
   };
@@ -73,7 +76,7 @@ export default function Home() {
         <div className="row mt-3">
           <div className="col-md-2">
             <h4 className="text-center">Filters By category</h4>
-            <div className="d-flex flex-wrap">
+            <div className="d-flex flex-column">
               {category?.map((c) => (
                 <Checkbox
                   key={c._id}
@@ -83,9 +86,27 @@ export default function Home() {
                 </Checkbox>
               ))}
             </div>
+            <h4 className="text-center mt-4">Filter By Price</h4>
+            <div className="d-flex flex-column">
+              <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+                {Prices?.map((p) => (
+                  <div key={p._id}>
+                    <Radio value={p.array}>{p.name}</Radio>
+                  </div>
+                ))}
+              </Radio.Group>
+            </div>
+            <div className="d-flex flex-column">
+              <button
+                className="btn btn-danger"
+                onClick={() => window.location.reload()}
+              >
+                RESET FILTERS
+              </button>
+            </div>
           </div>
+
           <div className="col-md-9">
-            {JSON.stringify(check, null, 4)}
             <h1 className="text-center">All products</h1>
             <div className="d-flex flex-wrap">
               {products?.map((p) => (
